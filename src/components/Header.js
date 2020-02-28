10c805a5fe9c585035da9006f89a4816803cfd54
adastraz <tylergosox@gmail.com>
2020-02-27 19:24:05 -0500

    added a simple favicon, we can chnage it later. Just need data now!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 const Header = props => {
     return (
@@ -11,8 +12,8 @@ const mapStateToProps = state => {
     return {
         isLoading: state.isLoading,
         user: state.user,
-        error: props.error
+        error: state.error
     }
 }
 
-export default (mapStateToProps, {})(Header)
+export default connect(mapStateToProps, {})(Header)
