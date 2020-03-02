9cb2d71901d2b8bdd144461320ff94d5783e79a7
Batuhan Balta <bthn.balta@gmail.com>
2020-03-01 22:28:24 -0800

    design

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
-    return (
-        <div> 
-            <h1>Welcome to How-To!</h1>
-            <nav>
-            <Link className='welcomeLinks' to="/signin">Sign in/Register</Link>
-          </nav>
-        </div>
-    )
-}
+  return (
+    <div className="welcome-howto">
+      <h1>Welcome to How-To!</h1>
+      <nav>
+        <Link className="welcomeLinks" to="/signin">
+          Sign in/Register
+        </Link>
+      </nav>
+    </div>
+  );
+};
 
-export default Welcome
+export default Welcome;
