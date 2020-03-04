0cd88fc43d213ad5e49d5cae837449ffa8da4d21
Batuhan Balta <bthn.balta@gmail.com>
2020-03-04 12:32:41 -0800

    create done

--- src/AxiosWithAuth.js
+++ src/AxiosWithAuth.js
@@ -4,7 +4,6 @@ export const axiosWithAuth = () =>
   axios.create({
     baseURL: "https://howto-be.herokuapp.com/api",
     headers: {
-      Authorization: localStorage.getItem("token"),
-      user_id: localStorage.getItem("user_id")
+      authorization: localStorage.getItem("token")
     }
   });
