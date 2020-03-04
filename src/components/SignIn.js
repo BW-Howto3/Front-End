722684ba6d488856157fc9e91981c77e2e1fb68e
Batuhan Balta <bthn.balta@gmail.com>
2020-03-03 23:47:24 -0800

    fix

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,7 @@
-import React, {useState, useEffect} from 'react'
-import { connect } from 'react-redux'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const SignIn = (props) => {
   const [user, setUser] = useState({
@@ -14,7 +13,6 @@ const SignIn = (props) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
-
   const submitLogin = () => {
     axios
       .post("https://howto-be.herokuapp.com/api/auth/login", user)
@@ -22,6 +20,7 @@ const SignIn = (props) => {
         console.log("login", result);
         localStorage.setItem("token", result.data.token);
         localStorage.setItem("user_id", result.data.user_id);
+        props.appRefresh();
         props.history.push("/howto");
       })
       .catch((error) => {
@@ -38,54 +37,53 @@ const SignIn = (props) => {
       });
   };
 
-    const submitRegister = e => {
-        axios.post('https://howto-be.herokuapp.com/api/auth/register', user)
-            .then(res => {
-                console.log('register' , res)
-                submitLogin()
-            })
-    }
-    const submitLogout = e => {
-        localStorage.clear()
-        props.appRefresh()
-    }
+  const submitLogout = (e) => {
+    localStorage.clear();
+    props.appRefresh();
+  };
 
-    useEffect(() => {
-        console.log(user)
-    }, [user])
-    return (
-        <header>
-            <nav>
-                <Link className='link' to="/about">About us</Link>
-                <Link className='link' to="/howto">How To Do It</Link>
-            </nav>
-           { localStorage.getItem("token")==null ? (<form onSubmit={ e => {
-                e.preventDefault() 
-            }}>
-                <label>Username</label>
-                <input
-                    name="username"
-                    value={user.username}
-                    onChange={e => handleChange(e)}
-                ></input>
-                <label>Password</label>
-                <input
-                    name="password"
-                    value={user.password}
-                    onChange={e => handleChange(e)}
-                ></input>
-                <button 
-                    type="submit"
-                    onClick={ e => submitLogin(e)} 
-                >Sign In</button>
-                <button
-                    onClick={ e => submitRegister(e)}
-                >Register</button>  
-            </form>) : (<button onClick={() => submitLogout()}>log out</button>) }
-        </header>
-    )
-}
-
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+  return (
+    <header>
+      <nav>
+        <Link className="link" to="/about">
+          About us
+        </Link>
+        <Link className="link" to="/howto">
+          How To Do It
+        </Link>
+      </nav>
+      {localStorage.getItem("token") == null ? (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <label>Username</label>
+          <input
+            name="username"
+            value={user.username}
+            onChange={(e) => handleChange(e)}
+          ></input>
+          <label>Password</label>
+          <input
+            name="password"
+            value={user.password}
+            onChange={(e) => handleChange(e)}
+          ></input>
+          <button type="submit" onClick={(e) => submitLogin(e)}>
+            Sign In
+          </button>
+          <button onClick={(e) => submitRegister(e)}>Register</button>
+        </form>
+      ) : (
+        <button onClick={() => submitLogout()}>log out</button>
+      )}
+    </header>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
